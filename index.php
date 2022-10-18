8c89b057851c4ffab6e09306bebe0a1878fdaa2f
Tahir Musharraf <tmusharraf6@gmail.com>
2022-10-18 10:08:33 +0500

    Update html-pdf library

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML Pages to PDF</title>
     <link rel="stylesheet" href="includes/style.css">
+    <script src="includes/html2pdf.js"></script>
 </head>
 <body>
     Hello World!
+
+    <div id="element-to-print">
+        <img src="includes/logo.png" alt=""> <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia impedit illo enim animi quis quo corporis beatae sequi perferendis iure labore temporibus a quas quidem tempora ex facilis nam aliquam praesentium quae ratione nihil assumenda, dignissimos eos. Dignissimos facere illo, excepturi similique corporis a praesentium nihil rem optio facilis quod eos pariatur dolor odio consequuntur, fuga dicta? Nesciunt id consectetur totam qui fugiat saepe libero dolorem. Et soluta odit, quo nam, aut modi vero cupiditate saepe, molestias illo sit minima quaerat. Asperiores optio sit, laudantium fuga blanditiis impedit facere dolorem nulla. Amet nesciunt voluptatibus quam? Inventore iusto repudiandae commodi omnis optio rerum at ipsa ut quae ullam. Accusantium officia vero enim! In ex accusamus, id voluptatum facere provident molestiae numquam repellat aliquam culpa! Nulla quisquam id esse! Debitis magni harum commodi autem assumenda fuga voluptatum adipisci, quia, dolorum eligendi, minus totam perferendis nostrum eveniet unde recusandae inventore! Beatae nesciunt, ducimus asperiores assumenda, hic itaque eum tenetur illo quas, harum eius distinctio. Alias, distinctio! Animi aut vel alias quibusdam fugit itaque hic tempore at necessitatibus inventore! Eum, ratione dolore! Assumenda ab adipisci provident nisi fugiat a, nulla iure commodi similique illum sit, amet aut obcaecati consequuntur molestiae enim eligendi ratione veniam.
+        <div class="html2pdf__page-break">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quam! Minus, eum dolorum autem eaque quaerat laboriosam saepe accusantium? Ad, aperiam? Necessitatibus itaque dolor qui fuga quidem suscipit corrupti quae, corporis nemo eum iusto possimus blanditiis enim sed nesciunt voluptatem quibusdam libero exercitationem magni ipsa! Pariatur consequatur, reiciendis iure error ducimus sequi aut quas doloremque, est, modi porro voluptates officia ipsum corrupti magnam ea excepturi sunt illo. Neque corporis culpa deleniti corrupti dolores tenetur sit nesciunt repellendus, enim, aliquid similique libero. Animi ipsa sequi iste nam? Quam similique quidem minima pariatur, facilis nostrum aut dolor laborum? Nesciunt incidunt iure quo, quisquam soluta adipisci. Praesentium laudantium dolor sed quisquam ullam nihil eos quas quos nulla vitae minus placeat, fugiat, saepe illum officiis nemo sunt nostrum magnam laboriosam error itaque! Placeat, facilis deserunt alias, odio debitis aliquid tempora et porro hic neque voluptas aspernatur exercitationem dolorum assumenda tempore vitae corrupti rem iusto optio adipisci unde, recusandae eligendi aliquam officia. Dicta libero porro officia inventore architecto voluptatem unde facilis distinctio quisquam eligendi, dignissimos sapiente vero repellendus et nobis dolorum eveniet nemo recusandae ipsum at illum enim doloremque consectetur? Expedita earum voluptates veritatis commodi, laboriosam quod aspernatur repudiandae vero, quidem suscipit quas modi aliquid.
+            <img src="includes/logo.png" />
+        </div>
+    </div>
 </body>
 <script src="includes/main.js" type="module"></script>
 </html>
